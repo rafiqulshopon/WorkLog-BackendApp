bdd9f6e5d388c2fe34a3d89b70fe3697ff108ed7
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-06-04 11:09:20 +0600

    feat: Update registration endpoint to use 'invite-register' route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -74,7 +74,7 @@ export class AuthController {
     }
   }
 
-  @Post('register')
+  @Post('invite-register')
   async registerInvitedUser(@Body() registerUserDto: RegisterUserDto) {
     try {
       return this.authService.registerInvitedUser(registerUserDto);
