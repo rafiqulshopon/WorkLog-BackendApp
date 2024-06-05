e99a82395f737ec6a24b76c2fd0b95bc1318baeb
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-06-06 02:28:21 +0600

    Update greeting message in AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): { message: string } {
-    return { message: 'Welcome to the auth project!' };
+    return { message: 'Welcome to the worklog!' };
   }
 }
